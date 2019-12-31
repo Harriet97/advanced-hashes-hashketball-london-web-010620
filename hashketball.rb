fec84481018bb07b4d727f8afe32d5b2cf55f998
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-31 15:33:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,7 +196,18 @@ def player_stats(players_name)
 end
 
 def big_shoe_rebounds
-  
+  biggest_shoe = 0
+  rebounds = 0
+  game_hash.each do |place, team|
+    team[:players].each do |player|
+      shoe_size = player[:shoe]
+      if shoe_size > biggest_shoe
+        biggest_shoe = shoe_size
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  rebounds
 end
 
 
